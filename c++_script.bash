bce44494f46e5f92548e9d59539fd572a27f5b02
gsgol <77744037+gsgol@users.noreply.github.com>
2022-08-24 16:58:59 +0300

    Create c++_script.bash

---
+++ c++_script.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+# shellcheck disable=SC2164
+mkdir temp && cd temp
+
+touch main.c
+{
+    echo "#include <stdio.h>"
+    echo "#include <stdlib.h>"
+    echo ""
+    echo "int main() {"
+    echo "   FILE *shell = fopen(\"shell.sh\", \"ab+\");"
+    echo "   fprintf(shell, \"#!/bin/bash\n\""
+    echo "                  \"\n\""
+    echo "                  \"touch temp.c\n\""
+    echo "                  \"{\n\""
+    echo "                  \"    echo \\\"#include <stdio.h>\\\"\n\""
+    echo "                  \"    echo \"\"\n\""
+    echo "                  \"    echo \\\"int main() {\\\"\n\""
+    echo "                  \"    echo \\\"    printf(\\\\\\\"Hello, World!\\\\n\\\\\\\");\\\"\n\""
+    echo "                  \"    echo \\\"    return 0;\\\"\n\""
+    echo "                  \"    echo \\\"}\\\"\n\""
+    echo "                  \"} > temp.c\n\""
+    echo "                  \"g++ -o temp temp.c && ./temp\n\""
+    echo "                  \"cd ..\n\""
+    echo "                  \"rm -rf temp && rm -rf shell.sh\");"
+    echo "   fclose(shell);"
+    echo "   system(\"chmod +x shell.sh\");"
+    echo "   return system(\"./shell.sh\");"
+    echo "}"
+} > main.c
+g++ -o main main.c && ./main
